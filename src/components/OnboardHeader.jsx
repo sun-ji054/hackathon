8e5e7b855ceb314dab85276f009b7f4f328460a2
sun-ji054 <[seonji04203645@gmail.com]>
2025-08-15 02:04:33 +0900

    design: onboardheader 수정 시작

--- src/components/OnboardHeader.jsx
+++ src/components/OnboardHeader.jsx
@@ -3,40 +3,34 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import OnboardBtn from "./OnboardBtn";
 
+const OnboardHeaderStyle = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  background-color: blue;
+`
+const Logo = styled.p`
+  font-size: xx-large;
+  line-height: 1;
+  margin: 0;
+`
+
 const ButtonGroup = styled.div`
   display: flex;
   margin-left: auto;
 `
 
+
 function OnboardHeader() {
   return(
-    <header className="sticky top-0 z-20 bg-white/90 backdrop-blur border-b">
-      <div className="flex items-center justify-between px-4 py-3">
-        <Link
-                    to="/"
-                    className="inline-flex items-center justify-center w-16 h-10 border rounded-lg font-semibold"
-                >
-                    logo
-                </Link>
+    <OnboardHeaderStyle>
+      <Logo><Link to="/">logo</Link></Logo>
       <ButtonGroup>
         <OnboardBtn btnName={'로그인'} page="loginPage"></OnboardBtn>
         <OnboardBtn btnName={'회원가입'} page="signUpPage"></OnboardBtn>
       </ButtonGroup>
-      </div>
-    </header>
+    </OnboardHeaderStyle>
   );
 }
 
 export default OnboardHeader;
-
-// const OnboardHeaderStyle = styled.header`
-//   display: flex;
-//   align-items: center;
-//   padding: 0 1rem;
-//   background-color: blue;
-// `
-// const Logo = styled.p`
-//   font-size: xx-large;
-//   line-height: 1;
-//   margin: 0;
-// `
