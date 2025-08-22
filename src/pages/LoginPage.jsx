c47d0062f9e8ec9406cadbce79a6da5dbcf1d149
sun-ji054 <[seonji04203645@gmail.com]>
2025-08-23 03:39:08 +0900

    feat:로그인 페이지 로고 삽입 -스크롤을 하는게 좋을지, 화면에 맞추는게 좋을지(시은 언니 화면에서 어떻게 나오는지 보기)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import LoginForm from "../components/LoginForm";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import loginTopImg from "../assets/icons/LoginTopImg.png";
 
-const Logo = styled.div`
+const Logo = styled.img`
   width: 100%;
-  height: 360px;
-  border-bottom: 1px dashed #CFCFCF;
+  height: auto;
 `
 const SignUp = styled.p`
   display: inline-block;
@@ -14,13 +14,14 @@ const SignUp = styled.p`
   font-weight: light;
   -webkit-text-fill-color: #8B6A55;
   border-bottom: 1px solid #8B6A55;
+  margin-bottom: 30px;
 `
 
 function LoginPage(){
   
   return(
-    <div style={{backgroundColor:'#FCFAF7', height: '100%'}}>
-      <Logo></Logo>
+    <div style={{backgroundColor:'#FCFAF7', height: '100vh', overflowY:'auto'}}>
+      <Logo src={loginTopImg} alt="로고" />
       <LoginForm></LoginForm>
       <div style={{display:'flex', justifyContent:'center'}}>
         <Link to='/signUpPage'>
