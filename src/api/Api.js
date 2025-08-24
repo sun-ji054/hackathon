f8e5ab01a7aa3d32a63b5fdf319693ec7f893d8d
sun-ji054 <[seonji04203645@gmail.com]>
2025-08-25 05:11:59 +0900

    feat:템플릿 검색 기능 구현

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://mypublishjy.site',
+    baseURL: 'https://hufs-likelion.store',
     headers: { 'Content-Type': 'application/json' },
 });
 
